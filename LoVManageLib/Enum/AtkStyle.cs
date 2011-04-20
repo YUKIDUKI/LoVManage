b6cd971eac73732e60f91344bf00a1421bc80341
yukiduki <yukiduki@1f8b95ef-c5fd-48de-8659-39393933249d>
2011-04-20 01:53:12 +0000

    ドキュメンテーションコメント追加
    git-svn-id: http://hanehaniwa.dip.jp/repos/LoVManage/trunk@7 1f8b95ef-c5fd-48de-8659-39393933249d

--- LoVManageLib/Enum/AtkStyle.cs
+++ LoVManageLib/Enum/AtkStyle.cs
@@ -5,10 +5,22 @@ using System.Text;
 
 namespace LoVManageLib.Enum
 {
+    /// <summary>
+    /// 攻撃形式
+    /// </summary>
     enum AtkStyle
     {
+        /// <summary>
+        /// 単数
+        /// </summary>
         SINGLE = 0,
+        /// <summary>
+        /// 複数
+        /// </summary>
         WIDE = 1,
+        /// <summary>
+        /// 拡散
+        /// </summary>
         SPREAD = 2
     }
 }
