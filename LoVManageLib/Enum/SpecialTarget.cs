f10b744d7da036ae94c1768fa31e97134dcaf001
yukiduki <yukiduki@1f8b95ef-c5fd-48de-8659-39393933249d>
2011-07-01 06:21:09 +0000

    SpecialTargetを定義
    git-svn-id: http://hanehaniwa.dip.jp/repos/LoVManage/trunk@19 1f8b95ef-c5fd-48de-8659-39393933249d

---
+++ LoVManageLib/Enum/SpecialTarget.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LoVManageLib.Enum
+{
+    /// <summary>
+    /// 特殊技の効果対象
+    /// </summary>
+    enum SpecialTarget
+    {
+        /// <summary>
+        /// 単数
+        /// </summary>
+        SINGLE = 1,
+        /// <summary>
+        /// 複数
+        /// </summary>
+        WIDE = 2,
+        /// <summary>
+        /// 自身
+        /// </summary>
+        SELF = 3,
+        /// <summary>
+        /// 施設
+        /// </summary>
+        FACILITIES = 4
+    }
+}
