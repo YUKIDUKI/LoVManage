77e4098efa9263ddbb4252f9492b9793cdf1913e
yukiduki <yukiduki@1f8b95ef-c5fd-48de-8659-39393933249d>
2011-09-16 06:38:25 +0000

    フィールドスキル列挙型定義
    git-svn-id: http://hanehaniwa.dip.jp/repos/LoVManage/trunk@40 1f8b95ef-c5fd-48de-8659-39393933249d

---
+++ LoVManageLib/Enum/FieldSkill.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LoVManageLib.Enum
+{
+    /// <summary>
+    /// フィールドスキル
+    /// </summary>
+    public enum FieldSkill
+    {
+        /// <summary>
+        /// ガード
+        /// </summary>
+        GUARD = 1,
+        /// <summary>
+        /// パワー
+        /// </summary>
+        POWER = 2,
+        /// <summary>
+        /// リペア
+        /// </summary>
+        REPAIR = 3,
+        /// <summary>
+        /// アルカナ
+        /// </summary>
+        ARCANA = 4,
+        /// <summary>
+        /// Wアルカナ
+        /// </summary>
+        W_ARCANA = 5,
+        /// <summary>
+        /// シールド
+        /// </summary>
+        SHIELD = 6,
+        /// <summary>
+        /// Wシールド
+        /// </summary>
+        W_SHIELD = 7,
+        /// <summary>
+        /// Tシールド
+        /// </summary>
+        T_SHIELD = 8,
+        /// <summary>
+        /// ゲート
+        /// </summary>
+        GATE = 9,
+        /// <summary>
+        /// Wゲート
+        /// </summary>
+        W_GATE = 10,
+        /// <summary>
+        /// Tゲート
+        /// </summary>
+        T_GATE = 11,
+        /// <summary>
+        /// サーチ
+        /// </summary>
+        SEARCH = 12,
+        /// <summary>
+        /// Wサーチ
+        /// </summary>
+        W_SEARCH = 13,
+        /// <summary>
+        /// Tサーチ
+        /// </summary>
+        T_SEARCH
+
+    }
+}
