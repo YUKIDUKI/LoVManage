8a64b0bd2b70176f2ee7cad275c03c8ea2fa9ed6
yukiduki <yukiduki@1f8b95ef-c5fd-48de-8659-39393933249d>
2011-09-16 08:28:55 +0000

    IDとNoと種族のプロパティを定義
    git-svn-id: http://hanehaniwa.dip.jp/repos/LoVManage/trunk@44 1f8b95ef-c5fd-48de-8659-39393933249d

--- LoVManageLib/Card.cs
+++ LoVManageLib/Card.cs
@@ -3,13 +3,26 @@ namespace LoVManageLib
 {
     public class Card
     {
+        private uint _id;
+        /// <summary>
+        /// ID
+        /// </summary>
+        public uint Id
+        {
+            set
+            {
+                if (this._id != value)
+                    this._id = value;
+            }
+            get { return this._id; }
+        }
         private string _name;
         /// <summary>
         /// カード名
         /// </summary>
         public string Name
         {
-            private set
+            set
             {
                 if (this._name != value)
                     this._name = value;
@@ -20,6 +33,34 @@ namespace LoVManageLib
         public Card(string name)
         {
             this.Name = name;
+        }
+
+        private string _no;
+        /// <summary>
+        /// No
+        /// </summary>
+        public string No
+        {
+            set
+            {
+                if (this._no != value)
+                    this._no = value;
+            }
+            get { return this._no; }
+        }
+
+        private Enum.Species _species;
+        /// <summary>
+        /// 種族
+        /// </summary>
+        public Enum.Species Species
+        {
+            set
+            {
+                if (this._species != value)
+                    this._species = value;
+            }
+            get { return this._species; }
         }
     }
 }
