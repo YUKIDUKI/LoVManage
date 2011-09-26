b776e5e45674e39939db3c4a1fbfd1b772ceb622
yukiduki <yukiduki@1f8b95ef-c5fd-48de-8659-39393933249d>
2011-09-26 04:18:19 +0000

    バージョン列挙型
    git-svn-id: http://hanehaniwa.dip.jp/repos/LoVManage/trunk@46 1f8b95ef-c5fd-48de-8659-39393933249d

---
+++ LoVManageLib/Enum/Version.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LoVManageLib.Enum
+{
+    /// <summary>
+    /// バージョン
+    /// </summary>
+    public enum Version
+    {
+        Ver1_0 = 1.0,
+        Ver1_1 = 1.1,
+        Ver1_2 = 1.2,
+        Ver1_3 = 1.3,
+        Ver1_4 = 1.4,
+        Ver2_0 = 2.0,
+        Ver2_1 = 2.1,
+        Ver2_5 = 2.5,
+        Ver2_6 = 2.6,
+        Ver2_7_Re2 = 2.7
+    }
+}
